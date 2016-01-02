1e5e41364e1fa23e6da31da9115ef9de378eeb61
Tennana <tennana@nifty.com>
2016-01-03 00:08:15 +0900

    Ruby 1.8用

--- src_ruby/pusherWarpper.rb
+++ src_ruby/pusherWarpper.rb
@@ -1,24 +1,13 @@
 require 'rubygems'
-require 'Pusher'
+require 'pusher'
 
 Pusher.url = $Pusher_API_URL
 
 module PusherWarpper
-	def test
-		logging('loginOnWebInterface Begin'+$Pusher_API_URL)
-		Pusher.trigger('test_channel', 'my_event', {
-		  message: 'hello world'
-		})
-	end
-	module_function :test
 	def auth(params)
-		logging('loginOnWebInterface Begin'+$Pusher_API_URL)
-		logging(params, "params")
 		result = Pusher.authenticate(params['channel_name'], params['socket_id'], {
-		  user_id: params['uniqueId'],
-		  user_info: {} # optional
+		  :user_id => params['uniqueId'],
 		})
-		logging(result);
 		return result;
 	end
 	module_function :auth
